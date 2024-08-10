e5d99b048732fd23269cdd9e103de51e441facae
seekplum <1131909224@qq.com>
2024-08-10 22:04:00 +0800

    MOD: 使用 form 表单展示 options.html

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Spin, Switch, Typography } from 'antd';
+import { Button, Form, Spin, Switch, Typography } from 'antd';
 import { Observer } from 'mobx-react-lite';
 import * as React from 'react';
 
@@ -13,43 +13,38 @@ const App: React.FC = () => {
 
     return (
         <Observer>
-            {() => (
-                <Flex justify="center" align="center">
-                    <Flex
-                        vertical
-                        gap={12}
-                        style={{
-                            width: 176,
-                            boxSizing: 'border-box',
-                            padding: '12px 0',
-                            userSelect: 'none',
-                        }}
+            {() =>
+                vm.initialized && !!vm.config ? (
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={vm.handleSubmit}
+                        autoComplete="off"
                     >
-                        {vm.initialized ? (
-                            <>
-                                <Flex justify="space-between">
-                                    <Typography.Text>版本</Typography.Text>
-                                    <Typography.Text>{VERSION}</Typography.Text>
-                                </Flex>
-                                <Flex justify="space-between">
-                                    <Typography.Text>是否折叠</Typography.Text>
-                                    <Switch
-                                        checked={vm.isCollapsed}
-                                        onChange={vm.onChangeCollapsed}
-                                    />
-                                </Flex>
-                                <Flex justify="end">
-                                    <Button type="primary" onClick={vm.handleSubmit}>
-                                        保存
-                                    </Button>
-                                </Flex>
-                            </>
-                        ) : (
-                            <Spin />
-                        )}
-                    </Flex>
-                </Flex>
-            )}
+                        <Form.Item label="版本" valuePropName="checked">
+                            <Typography.Text>{VERSION}</Typography.Text>
+                        </Form.Item>
+                        <Form.Item
+                            label="是否折叠"
+                            name="isCollapsed"
+                            initialValue={!vm.config.isCollapsed}
+                            valuePropName="checked"
+                        >
+                            <Switch />
+                        </Form.Item>
+                        <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+                            <Button type="primary" htmlType="submit">
+                                保存
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                ) : (
+                    <Spin />
+                )
+            }
         </Observer>
     );
 };
